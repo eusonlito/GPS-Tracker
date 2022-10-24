3a04119288d7799d665f914cdf2e7f21e245d5f2
Lito <lito@eordes.com>
2022-10-24 23:08:30 +0200

    Improved distanceHuman

--- app/Services/Helper/Traits/Number.php
+++ app/Services/Helper/Traits/Number.php
@@ -56,6 +56,14 @@ trait Number
      */
     public function distanceHuman(int $meters, int $decimals = 2): string
     {
-        return $this->number(($km = ($meters >= 1000)) ? ($meters / 1000) : $meters, $decimals).' '.($km ? 'km' : 'm');
+        if ($meters >= 1000) {
+            $meters /= 1000;
+            $units = 'km';
+        } else {
+            $decimals = 0;
+            $units = 'm';
+        }
+
+        return $this->number($meters, $decimals).' '.$units;
     }
 }
