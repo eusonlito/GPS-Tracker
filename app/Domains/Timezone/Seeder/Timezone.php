dd2e6a1094943d0fbc6c160f796931015cb08879
Lito <lito@eordes.com>
2022-11-03 09:24:57 +0100

    Updated Timezone Seeder and Database Dump

--- app/Domains/Timezone/Seeder/Timezone.php
+++ app/Domains/Timezone/Seeder/Timezone.php
@@ -2,6 +2,7 @@
 
 namespace App\Domains\Timezone\Seeder;
 
+use Illuminate\Database\Query\Expression;
 use App\Domains\Timezone\Model\Timezone as Model;
 use App\Domains\Shared\Seeder\SeederAbstract;
 
@@ -12,6 +13,26 @@ class Timezone extends SeederAbstract
      */
     public function run()
     {
-        $this->insertWithoutDuplicates(Model::class, $this->json('timezone'), 'zone');
+        $this->insertWithoutDuplicates(Model::class, $this->map($this->json('timezone')), 'zone');
+    }
+
+    /**
+     * @param array $list
+     *
+     * @return void
+     */
+    protected function map(array $list)
+    {
+        $geojson = $this->geojson();
+
+        return array_map(static fn ($row) => ['geojson' => $geojson] + $row, $list);
+    }
+
+    /**
+     * @return \Illuminate\Database\Query\Expression
+     */
+    protected function geojson(): Expression
+    {
+        return Model::DB()->raw(Model::emptyGeoJSON());
     }
 }
