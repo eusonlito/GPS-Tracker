0bf166e7c96297075e12f0a93efde6746026728d
Lito <lito@eordes.com>
2022-11-05 22:51:32 +0100

    Set position trip_id as not null

---
+++ database/migrations/2022_11_05_220000_position_trip_id.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Domains\Shared\Migration\MigrationAbstract;
+
+return new class extends MigrationAbstract
+{
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        $this->map();
+        $this->tables();
+        $this->upFinish();
+    }
+
+    /**
+     * @return void
+     */
+    protected function map()
+    {
+        $this->db()->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('point', 'array');
+    }
+
+    /**
+     * @return void
+     */
+    protected function tables()
+    {
+        $this->db()->statement('DELETE FROM `position` WHERE `trip_id` IS NULL;');
+
+        Schema::table('position', function (Blueprint $table) {
+            $this->tableDropForeign($table, 'trip', 'fk');
+            $this->foreignOnDeleteCascade($table, 'trip');
+        });
+
+        Schema::table('position', function (Blueprint $table) {
+            $table->unsignedBigInteger('trip_id')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down()
+    {
+        $this->map();
+
+        Schema::table('position', function (Blueprint $table) {
+            $table->unsignedBigInteger('trip_id')->nullable(true)->change();
+        });
+
+        Schema::table('position', function (Blueprint $table) {
+            $this->tableDropForeign($table, 'trip', 'fk');
+            $this->foreignOnDeleteSetNull($table, 'trip');
+        });
+    }
+};
