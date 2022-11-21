99e42ad320a42cb01d2eaf6fc497df1682174c03
Lito <lito@eordes.com>
2022-11-21 16:54:46 +0100

    Fixed Device Alarm Notification Alerts

--- resources/views/domains/device-alarm-notification/molecules/alert.blade.php
+++ resources/views/domains/device-alarm-notification/molecules/alert.blade.php
@@ -1,27 +1,31 @@
 <app-message class="show flex items-center mb-2 mt-2 alert alert-danger">
-    @icon('alert-circle', 'w-6 h-6 mr-2')
+    <div class="hidden lg:block">
+        @icon('alert-circle', 'w-6 h-6 mr-2')
+    </div>
 
-    <strong>
-        <a href="{{ route('device.update', $row->device->id) }}">{{ $row->device->name }}</a>
+    <div class="mr-4">
+        <strong>
+            <a href="{{ route('device.update', $row->device->id) }}">{{ $row->device->name }}</a>
+
+            <span class="mx-2">-</span>
+
+            <a href="{{ route('device.update.device-alarm.update', [$row->device->id, $row->alarm->id]) }}">
+                {{ $row->alarm->name }}
+                -
+                {{ $row->typeFormat()->title() }}
+            </a>
+
+            @if ($row->trip)
+            <span class="mx-2">-</span>
+
+            <a href="{{ route('trip.update.map', $row->trip->id) }}#position-id-{{ $row->position?->id }}">{{ $row->trip->name }}</a>
+            @endif
+        </strong>
 
         <span class="mx-2">-</span>
 
-        <a href="{{ route('device.update.device-alarm.update', [$row->device->id, $row->alarm->id]) }}">
-            {{ $row->alarm->name }}
-            -
-            {{ $row->typeFormat()->title() }}
-        </a>
-
-        @if ($row->trip)
-        <span class="mx-2">-</span>
-
-        <a href="{{ route('trip.update.map', $row->trip->id) }}#position-id-{{ $row->position?->id }}">{{ $row->trip->name }}</a>
-        @endif
-    </strong>
-
-    <span class="mx-2">-</span>
-
-    <a href="{{ route('device.update.device-alarm-notification', $row->device->id) }}">{{ $row->typeFormat()->message() }}</a>
+        <a href="{{ route('device.update.device-alarm-notification', $row->device->id) }}">{{ $row->typeFormat()->message() }}</a>
+    </div>
 
     <a href="{{ route('device-alarm-notification.update.closed-at', $row->id) }}" class="btn-close">
         @icon('x', 'w-4 h-4')
