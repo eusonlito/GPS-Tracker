444e67fa031ab38d73540e2abddb8b0e13e3c9bb
Lito <lito@eordes.com>
2022-12-19 22:26:20 +0100

    Updated value2color

--- resources/views/assets/js/value2color.js
+++ resources/views/assets/js/value2color.js
@@ -1,39 +1,24 @@
-export default function (value, minvalue = 0, maxvalue = 10, minColor, maxColor, underMinColor = 'rgb(155,0,0)', upperMaxColor = 'rgb(200,250,240)') {
-    function colorStrToArray(colorObj) {
-        if (Array.isArray(colorObj) && colorObj.length == 3) {
-            return colorObj;
-        }
+function rgb(r, g, b) {
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
-        if (typeof colorObj !== 'string') {
-            return null;
-        }
+const colorMin = [255, 0, 0];
+const colorMax = [0, 255, 0];
 
-        if (colorObj[0] === '#') {
-            return [parseInt(colorObj.substring(1, 3), 16), parseInt(colorObj.substring(3, 5), 16), parseInt(colorObj.substring(5), 16)];
-        }
-
-        if (colorObj[0] === 'r') {
-            return colorObj.substring(4, colorObj.length - 1).split(',').map(str => parseInt(str));
-        }
-
-        return null;
+export default function (value, valueMin, valueMax) {
+    if (value >= valueMax) {
+        return rgb(...colorMax);
     }
 
-    minColor = colorStrToArray(minColor) || [255, 0, 0];
-    maxColor = colorStrToArray(maxColor) || [0, 255, 0];
-
-    if (value >= maxvalue) {
-        return upperMaxColor;
+    if (value <= valueMin) {
+        return rgb(...colorMin);
     }
 
-    if (value <= minvalue) {
-        return underMinColor;
-    }
+    const d = (value - valueMin) / Math.abs(valueMax - valueMin);
 
-    const d = (value - minvalue) / Math.abs(maxvalue - minvalue);
-    let r = Math.round(minColor[0] + (maxColor[0] - minColor[0]) * d);
-    let g = Math.round(minColor[1] + (maxColor[1] - minColor[1]) * d);
-    let b = Math.round(minColor[2] + (maxColor[2] - minColor[2]) * d);
-
-    return `rgb(${r},${g},${b})`;
+    return rgb(
+        Math.round(colorMin[0] + (colorMax[0] - colorMin[0]) * d),
+        Math.round(colorMin[1] + (colorMax[1] - colorMin[1]) * d),
+        Math.round(colorMin[2] + (colorMax[2] - colorMin[2]) * d)
+    );
 }
