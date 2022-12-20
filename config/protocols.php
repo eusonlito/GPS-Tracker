5b0df604a0962db3cb91333879369fb24a236ab2
Lito <lito@eordes.com>
2022-12-20 20:50:52 +0100

    [WIP] GT06 Protocol

--- config/protocols.php
+++ config/protocols.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Services\Protocol\GT06\Manager as GT06Manager;
 use App\Services\Protocol\H02\Manager as H02Manager;
 use App\Services\Protocol\OsmAnd\Manager as OsmAndManager;
 
 return [
+    'gt06' => GT06Manager::class,
     'h02' => H02Manager::class,
     'osmand' => OsmAndManager::class,
 ];
