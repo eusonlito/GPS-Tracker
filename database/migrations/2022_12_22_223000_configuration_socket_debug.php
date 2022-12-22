2c9c2feddf881b040e932408eeacabe838b2a7eb
Lito <lito@eordes.com>
2022-12-22 09:50:55 +0100

    Added 2022_12_22_223000_configuration_socket_debug migration

---
+++ database/migrations/2022_12_22_223000_configuration_socket_debug.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+use App\Domains\SharedApp\Migration\MigrationAbstract;
+
+return new class extends MigrationAbstract
+{
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        $this->db()->statement('
+            DELETE FROM `configuration`
+            WHERE `key` = "socket_debug"
+            LIMIT 1;
+        ');
+    }
+};
