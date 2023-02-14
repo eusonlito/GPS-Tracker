d98b3174076734bc6edf079eaec249125ddd1b72
Lito <lito@eordes.com>
2023-02-15 00:16:00 +0100

    Improved Docker Deploy

--- docker/init.sh
+++ docker/init.sh
@@ -2,8 +2,6 @@
 
 su -s /bin/bash -c 'composer deploy-docker' www-data
 
-su -s /bin/bash -c 'php artisan server:start:all' www-data
-
 cron
 
 su -s /bin/bash -c 'php artisan serve --host=0.0.0.0 --port=80' www-data
