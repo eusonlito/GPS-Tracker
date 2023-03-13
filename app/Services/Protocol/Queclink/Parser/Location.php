9ee800a25a566b1fc010a0fb4458d12f80217061
Lito <lito@eordes.com>
2023-03-13 21:44:46 +0100

    Updated Queclink Protocol Parser. Related #6

--- app/Services/Protocol/Queclink/Parser/Location.php
+++ app/Services/Protocol/Queclink/Parser/Location.php
@@ -49,24 +49,24 @@ class Location extends ParserAbstract
     {
         return '/^'
             .'\+(RESP|BUFF):GTFRI,'        //  0 - response code
-            .'[A-Z0-9]+,'                  //  1 - protocol version
+            .'[0-9A-Z]+,'                  //  1 - protocol version
             .'[0-9]{15},'                  //  2 - serial
             .'[0-9A-Za-z_-]{0,20},'        //  3 - device name
             .','                           //  4 - reserved
             .'[0-9]{0,5},'                 //  5 - external power voltage
             .'[0-9]{2},'                   //  6 - report id
-            .'[0-9]{1},'                   //  7 - number
+            .'[0-9]{1,2},'                 //  7 - number
             .'[0-9]{1,2},'                 //  8 - gnss accuracy
             .'[0-9]{1,3}\.[0-9],'          //  9 - speed
-            .'[0-9]{3},'                   // 10 - direction
+            .'[0-9]{1,3},'                 // 10 - direction
             .'\-?[0-9]{1,5}\.[0-9],'       // 11 - altitude
             .'\-?[0-9]{1,3}\.[0-9]{0,6},'  // 12 - longitude
             .'\-?[0-9]{1,2}\.[0-9]{0,6},'  // 13 - latitude
             .'[0-9]{14},'                  // 14 - utc time
-            .'[0-9]{0,4},'                 // 15 - mcc
-            .'[0-9]{0,4},'                 // 16 - mnc
-            .'[0-9]{0,4},'                 // 17 - lac
-            .'[A-Z0-9]{0,8},'              // 18 - cell id
+            .'[0-9A-Z]{0,4},'              // 15 - mcc
+            .'[0-9A-Z]{0,4},'              // 16 - mnc
+            .'[0-9A-Z]{0,4},'              // 17 - lac
+            .'[0-9A-Z]{0,8},'              // 18 - cell id
             .'/';
     }
 
