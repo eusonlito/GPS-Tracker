754e4d86e451d3e5c13601f349f09182b9874188
Lito <lito@eordes.com>
2023-09-14 16:31:56 +0200

    Unified PHP CS Fixer and Pint

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,12 +8,23 @@ return (new Config())
     ->setRules([
         '@PHP82Migration' => true,
         '@PSR2' => true,
+        'blank_line_after_opening_tag' => false,
+        'cast_spaces' => false,
         'class_definition' => false,
+        'linebreak_after_opening_tag' => false,
+        'no_superfluous_phpdoc_tags' => false,
         'no_unused_imports' => true,
+        'not_operator_with_successor_space' => false,
+        'nullable_type_declaration_for_default_null_value' => true,
+        'ordered_imports' => false,
+        'phpdoc_order' => true,
         'phpdoc_separation' => true,
+        'single_line_comment_style' => false,
+
         'curly_braces_position' => [
-            'anonymous_classes_opening_brace' => 'same_line',
             'allow_single_line_empty_anonymous_classes' => true,
+
+            'anonymous_functions_opening_brace' => 'same_line',
         ],
     ])
     ->setFinder(Finder::create()->in(['app', 'config', 'database', 'tests']));
