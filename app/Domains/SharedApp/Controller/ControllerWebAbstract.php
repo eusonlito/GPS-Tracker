f41fdbc20bbe362cb771f30b2ba8343f6d450142
Lito <lito@eordes.com>
2023-09-25 10:25:44 +0200

    Added Execution Time and Memory Usage as footer comment

--- app/Domains/SharedApp/Controller/ControllerWebAbstract.php
+++ app/Domains/SharedApp/Controller/ControllerWebAbstract.php
@@ -6,4 +6,14 @@ use App\Domains\Shared\Controller\ControllerWebAbstract as SharedControllerWebAb
 
 abstract class ControllerWebAbstract extends SharedControllerWebAbstract
 {
+    /**
+     * @return void
+     */
+    protected function initCustom(): void
+    {
+        view()->share([
+            'MEMORY_USAGE' => helper()->sizeHuman(memory_get_usage(false)),
+            'EXECUTION_TIME' => sprintf('%.3f', microtime(true) - LARAVEL_START, 3),
+        ]);
+    }
 }
--- resources/views/layouts/molecules/footer.blade.php
+++ resources/views/layouts/molecules/footer.blade.php
@@ -1,1 +1,3 @@
 <script src="@asset('build/js/main.min.js')"></script>
+
+<!-- Execution Time {{ $EXECUTION_TIME }} seconds // Memory Usage {{ $MEMORY_USAGE }} -->
