26f81eb415e3046808afadc27baffb66b3400fa1
Lito <lito@eordes.com>
2023-11-19 00:31:49 +0100

    Updated demo header

--- resources/views/layouts/molecules/demo-header.blade.php
+++ resources/views/layouts/molecules/demo-header.blade.php
@@ -1,8 +1,7 @@
 @if (config('demo.enabled'))
 
 <div class="demo-header">
-    {{ __('demo.header.countdown', ['time' => helper()->dateDiffHuman(time(), date('Y-m-d '.config('demo.reset_time'), strtotime('tomorrow')))]) }}
-    - <a href="https://github.com/eusonlito/GPS-Tracker" target="_blank">GitHub</a>
+    {{ __('demo.header.countdown', ['time' => helper()->dateDiffHuman(time(), strtotime('tomorrow '.config('demo.reset_time')))]) }}
 </div>
 
 @endif
