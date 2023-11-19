b82fa7d22399f2fff05e868e571a97ef109797ef
Lito <lito@eordes.com>
2023-11-19 20:25:57 +0100

    Improved Demo Header

--- resources/views/layouts/molecules/demo-header.blade.php
+++ resources/views/layouts/molecules/demo-header.blade.php
@@ -1,7 +1,8 @@
 @if (config('demo.enabled'))
 
 <div class="demo-header">
-    {{ __('demo.header.countdown', ['time' => helper()->demoTimeToReset()]) }}
+    {{ __('demo.header.countdown', ['time' => helper()->demoTimeToReset()]) }}.
+    <a href="https://github.com/eusonlito/GPS-Tracker" target="_blank">View at Github</a>
 </div>
 
 @endif
