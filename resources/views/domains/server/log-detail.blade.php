d7774ce732d86fd5d055d0ad1a8139ec3e64c26e
Lito <lito@eordes.com>
2024-01-08 13:50:08 +0100

    Added Server Log Scroll

--- resources/views/domains/server/log-detail.blade.php
+++ resources/views/domains/server/log-detail.blade.php
@@ -7,6 +7,7 @@
 <div class="mb-5 text-center">
     <button href="#" data-copy="#log-contents" class="btn bg-white">@icon('clipboard', 'w-5 h-5')</button>
     <button href="#" data-select="#log-contents" class="btn bg-white ml-5">@icon('check-square', 'w-5 h-5')</button>
+    <button href="#" data-scroll-bottom="#log-contents" class="btn bg-white ml-5">@icon('chevrons-down', 'w-5 h-5')</button>
 </div>
 
 <pre id="log-contents" class="p-2 bg-white w-full max-h-screen overflow-x-auto">{{ $contents }}</pre>
