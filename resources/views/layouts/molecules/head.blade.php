ab969651a010486ca30fe543094af42504f0cbb3
Lito <lito@eordes.com>
2024-01-19 01:24:36 +0100

    Disabled Night Mode. Related #39

--- resources/views/layouts/molecules/head.blade.php
+++ resources/views/layouts/molecules/head.blade.php
@@ -5,7 +5,6 @@
 <meta name="apple-mobile-web-app-status-bar-style" content="white-translucent">
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}" />
 <meta name="application-name" content="{{ config('app.name') }}" />
-<meta name="color-scheme" content="light only">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="mobile-web-app-capable" content="yes" />
 <meta name="robots" content="none, noindex, nofollow, noarchive, nosnippet" />
