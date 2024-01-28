0391fe7a9b651e584b33572110ce16467ae72499
Lito <lito@eordes.com>
2024-01-28 15:56:14 +0100

    Updated Translations

--- resources/lang/fr/trip-import.php
+++ resources/lang/fr/trip-import.php
@@ -1,10 +1,10 @@
 <?php return [
     'device' => 'Appareil',
     'error' => [
-        'device-not-found' => 'L\'appareil sélectionné n\'est plus disponible',
+        'device_id-exists' => 'L\'appareil sélectionné n\'est plus disponible',
         'points-empty' => 'Aucun point n\'a pu être obtenu à partir du fichier GPX. Peut-être le fichier ne comporte-t-il pas d\'horodatage?',
         'timezone_id-exists' => 'Le fuseau horaire sélectionné n\'est plus disponible',
-        'vehicle-not-found' => 'Le véhicule sélectionné n\'est plus disponible',
+        'vehicle_id-exists' => 'Le véhicule sélectionné n\'est plus disponible',
     ],
     'file' => 'Fichier GPX',
     'meta-title' => 'Voyages > Importer',
