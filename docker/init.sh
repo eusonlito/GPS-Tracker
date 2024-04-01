05cf285e0c92898e06df76a24db4258812471e36
Lito <lito@eordes.com>
2024-04-01 18:05:31 +0200

    Updated Docker

--- docker/init.sh
+++ docker/init.sh
@@ -4,4 +4,4 @@ su -s /bin/bash -c './composer deploy-docker' www-data
 
 cron
 
-su -s /bin/bash -c 'php artisan serve --host=0.0.0.0 --port=80' www-data
+su -s /bin/bash -c 'php artisan serve --host=0.0.0.0 --port=80 > storage/logs/artisan-serve.log 2>&1' www-data
