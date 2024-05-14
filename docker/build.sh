be4ce57eff8813042e3c5bec968d81280cb757e9
Lito <lito@eordes.com>
2024-05-14 16:50:06 +0200

    Updated docker commands

--- docker/build.sh
+++ docker/build.sh
@@ -5,4 +5,5 @@ sudo rm -rf bootstrap/cache/*.php
 cp -n docker/.env.example .env
 cp -n docker/docker-compose.yml.example docker/docker-compose.yml
 
-sudo docker compose -f docker/docker-compose.yml build
+sudo docker-compose -f docker/docker-compose.yml pull
+sudo docker-compose -f docker/docker-compose.yml up --force-recreate --build -d
--- docker/stop.sh
+++ docker/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker compose -f docker/docker-compose.yml stop
+sudo docker-compose -f docker/docker-compose.yml stop
