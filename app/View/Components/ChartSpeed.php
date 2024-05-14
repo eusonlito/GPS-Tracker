d9056a90db7aac47b1a20d69c05aae451c221e18
Lito <lito@eordes.com>
2024-05-14 12:40:13 +0200

    Added Trip Speed Chart

---
+++ app/View/Components/ChartSpeed.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\View\View;
+use App\Domains\Alarm\Model\Collection\Alarm as AlarmCollection;
+use App\Domains\AlarmNotification\Model\Collection\AlarmNotification as AlarmNotificationCollection;
+use App\Domains\Position\Model\Collection\Position as PositionCollection;
+use App\Domains\Position\Model\Position as PositionModel;
+use App\Domains\Trip\Model\Trip as TripModel;
+
+class ChartSpeed extends Component
+{
+    /**
+     * @param \App\Domains\Position\Model\Collection\Position $positions
+     *
+     * @return self
+     */
+    public function __construct(readonly public PositionCollection $positions) {
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldRender(): bool
+    {
+        return $this->positions->isNotEmpty();
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function render(): View
+    {
+        return view('components.chart-speed', [
+            'id' => 'chart_speed_'.uniqid(),
+            'positionsJson' => $this->positionsJson(),
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    protected function positionsJson(): string
+    {
+        return $this->positions
+            ->toBase()
+            ->sortBy('date_at')
+            ->map($this->positionsJsonMap(...))
+            ->values()
+            ->toJson();
+    }
+
+    /**
+     * @param \App\Domains\Position\Model\Position $position
+     *
+     * @return array
+     */
+    protected function positionsJsonMap(PositionModel $position): array
+    {
+        return [
+            'id' => $position->id,
+            'date_at' => explode(' ', $position->date_at)[1],
+            'speed' => helper()->unit('speed', $position->speed),
+        ];
+    }
+}
