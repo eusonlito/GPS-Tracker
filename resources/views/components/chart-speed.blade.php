6de6df3428578bf86234bb4a07f77a88f91dac97
Lito <lito@eordes.com>
2024-05-23 00:11:59 +0200

    Removed chart.js from js main build

--- resources/views/components/chart-speed.blade.php
+++ resources/views/components/chart-speed.blade.php
@@ -1,72 +1,73 @@
-<script>
-const positions = {!! $positionsJson !!};
+@once
 
-const charts = [
-    {
-        id: '{{ $id }}',
-        config: {
-            type: 'line',
-            data: {
-                labels: positions.map(position => position.date_at),
-                datasets: [
-                    {
-                        label: '{{ __('map.speed') }}',
-                        data: positions.map(position => position.speed),
-                        pointRadius: 0,
-                        fill: false,
-                        pointRadius: 0,
-                        pointHitRadius: 10,
-                        steppedLine: false,
-                        borderColor: '#FF5252',
-                        borderWidth: 2,
-                        tension: 0.4
-                    }
-                ]
-            },
-            options: {
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                },
-                scales: {
-                    x: {
-                        ticks: {
-                            autoSkip: true
-                        },
-                        grid: {
-                            display: false
-                        }
-                    },
+<script src="@asset('build/js/chart.js')"></script>
 
-                    y: {
-                        grace: '10%',
-                        beginAtZero: true,
-
-                        ticks: {
-                            padding: 5
-                        }
-                    },
-                },
-                onClick: function (e, item) {
-                    const index = item[0].index;
-
-                    if (!positions[index]) {
-                        return;
-                    }
-
-                    const element = document.querySelector('[data-map-point="' + positions[index].id + '"]');
-
-                    if (element) {
-                        element.click();
-                    }
-                }
-            }
-        }
-    }
-];
-</script>
+@endonce
 
 <div class="box mt-5 p-5">
     <canvas id="{{ $id }}" height="60"></canvas>
 </div>
+
+<script>
+const positions = {!! $positionsJson !!};
+
+new Chart(document.getElementById('{{ $id }}'), {
+    type: 'line',
+    data: {
+        labels: positions.map(position => position.date_at),
+        datasets: [
+            {
+                label: '{{ __('map.speed') }}',
+                data: positions.map(position => position.speed),
+                pointRadius: 0,
+                fill: false,
+                pointRadius: 0,
+                pointHitRadius: 10,
+                steppedLine: false,
+                borderColor: '#FF5252',
+                borderWidth: 2,
+                tension: 0.4
+            }
+        ]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    autoSkip: true
+                },
+                grid: {
+                    display: false
+                }
+            },
+
+            y: {
+                grace: '10%',
+                beginAtZero: true,
+
+                ticks: {
+                    padding: 5
+                }
+            },
+        },
+        onClick: function (e, item) {
+            const index = item[0].index;
+
+            if (!positions[index]) {
+                return;
+            }
+
+            const element = document.querySelector('[data-map-point="' + positions[index].id + '"]');
+
+            if (element) {
+                element.click();
+            }
+        }
+    }
+});
+</script>
