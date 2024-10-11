65eca0c2643a70913bf41ad9b4a7a5630c9357c3
elvis <slv_evs@yahoo.com.br>
2024-10-10 23:36:40 -0300

    translated some more of the alarm lang files

---
+++ resources/lang/pt_BR/alarm-notification-update.php
@@ -0,0 +1,3 @@
+<?php return [
+    'delete-success' => 'A notificação foi deletada com sucesso',
+];
---
+++ resources/lang/pt_BR/alarm-type-fence-in.php
@@ -0,0 +1,12 @@
+<?php return [
+    'error' => [
+        'latitude' => 'A Latitude indicada não tem um valor válido',
+        'longitude' => 'The Longitude indicated does not have a valid value',
+        'radius' => 'O Raio indicado não tem um valor válido',
+    ],
+    'latitude' => 'Latitude',
+    'longitude' => 'Longitude',
+    'message' => 'O veículo entrou na cerca',
+    'radius' => 'Raio em KM',
+    'title' => 'Cerca (Entry)',
+];
