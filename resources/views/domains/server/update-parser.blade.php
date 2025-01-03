caeace6a18aff76ff8ebf59a4cadc8793db6d9dd
Lito <lito@eordes.com>
2025-01-03 01:17:31 +0100

    Added Aquila Protocol

--- resources/views/domains/server/update-parser.blade.php
+++ resources/views/domains/server/update-parser.blade.php
@@ -68,8 +68,8 @@
 
                 @if ($resource->format() === 'location')
 
-                <td>{{ $resource->latitude() }}</td>
-                <td>{{ $resource->longitude() }}</td>
+                <td><a href="https://maps.google.com/?q={{ $resource->latitude() }},{{ $resource->longitude() }}" rel="nofollow noopener noreferrer" target="_blank">{{ $resource->latitude() }}</a></td>
+                <td><a href="https://maps.google.com/?q={{ $resource->latitude() }},{{ $resource->longitude() }}" rel="nofollow noopener noreferrer" target="_blank">{{ $resource->longitude() }}</a></td>
                 <td>{{ $resource->speed() }}</td>
                 <td>{{ $resource->direction() }}</td>
                 <td>{{ $resource->signal() }}</td>
