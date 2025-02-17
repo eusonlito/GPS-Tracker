c9cf6dc8b63345295de216ec6ae1d08b8260e4b7
Lito <lito@eordes.com>
2025-02-17 10:10:52 +0100

    Improved Language Management

--- app/Providers/App.php
+++ app/Providers/App.php
@@ -14,41 +14,8 @@ class App extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->locale();
-        $this->language();
         $this->configuration();
-    }
-
-    /**
-     * @return void
-     */
-    protected function locale(): void
-    {
-        $locale = config('app.locale');
-
-        if (str_contains($locale, '_')) {
-            return;
-        }
-
-        $new = match ($locale) {
-            'en' => 'en_US',
-            'pt' => 'pt_BR',
-            'he' => 'he_IL',
-            'ar' => 'ar_AE',
-            default => $locale.'_'.strtoupper($locale),
-        };
-
-        trigger_error(sprintf('Configure APP_LOCALE as "%s", currently is "%s"', $new, $locale), E_USER_DEPRECATED);
-
-        config(['app.locale' => $new]);
-    }
-
-    /**
-     * @return void
-     */
-    protected function language(): void
-    {
-        $this->factory('Language')->action()->set();
+        $this->language();
     }
 
     /**
@@ -57,5 +24,13 @@ class App extends ServiceProvider
     protected function configuration(): void
     {
         $this->factory('Configuration')->action()->appBind();
+    }
+
+    /**
+     * @return void
+     */
+    protected function language(): void
+    {
+        $this->factory('Language')->action()->set();
     }
 }
