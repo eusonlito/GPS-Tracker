84f9e063dbd973a9d5007a8d102e5568e3cdeeb1
1ien4nh <anhnt@goads.vn>
2025-02-20 14:52:10 +0700

    add tittle roles

--- app/Domains/Role/Controller/router.php
+++ app/Domains/Role/Controller/router.php
@@ -9,6 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['user-auth']], function () {
     Route::get('/role', Index::class)->name('role.index');
-    // Route::get('/role/{id}/edit', Edit::class)->name('role.edit');
-    // Route::get('/role/create', Create::class)->name('role.create');
+    Route::get('/role/{id}/edit', Edit::class)->name('role.edit');
+    Route::get('/role/create', Create::class)->name('role.create');
 });
--- resources/lang/en_US/role-index.php
+++ resources/lang/en_US/role-index.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'meta-title' => 'Roles',
     'Search' => 'Search',
     'Search...' => 'Search...',
     'ID' => 'ID',
