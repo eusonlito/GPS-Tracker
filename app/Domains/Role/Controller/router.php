382a787d04d74f24a80d34fffeefaef1580ce1ce
1ien4nh <anhnt@goads.vn>
2025-02-20 19:36:31 +0700

    thêm lại create role

--- app/Domains/Role/Controller/router.php
+++ app/Domains/Role/Controller/router.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 use App\Domains\Role\Controller\Index as RoleIndex;
 use App\Domains\Role\Feature\Controller\Index as FeatureIndex;
+use App\Domains\Role\Controller\Create;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['user-auth']], function () {
     Route::get('/role', RoleIndex::class)
         ->name('role.index');
-
+    Route::get('/role/create', Create::class)->name('role.create');
+    Route::post('/role/store', [Create::class, 'store'])->name('role.store');
     Route::get('/role/feature', FeatureIndex::class)
         ->name('role.feature.index')
         ->middleware('user.role.feature.access:role-feature'); // Chỉ manager mới có quyền
