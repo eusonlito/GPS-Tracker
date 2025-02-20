b94bc5d0fde247b2261e7ae664c01a2a079e090a
PHAM THANH PHONG <106043557+phongpham663@users.noreply.github.com>
2025-02-20 11:46:51 +0700

    add base user link to role

--- app/Domains/User/Model/User.php
+++ app/Domains/User/Model/User.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Domains\User\Model;
 
@@ -15,6 +17,8 @@ use App\Domains\User\Model\Collection\User as Collection;
 use App\Domains\User\Model\Traits\Preferences as PreferencesTrait;
 use App\Domains\User\Test\Factory\User as TestFactory;
 use App\Domains\UserSession\Model\UserSession as UserSessionModel;
+use App\Domains\Role\Model\Role as RoleModel;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends ModelAbstract implements Authenticatable
 {
@@ -117,5 +121,17 @@ class User extends ModelAbstract implements Authenticatable
     public function managerMode(): bool
     {
         return $this->manager && $this->manager_mode;
+    }
+
+    // Thêm quan hệ đến Role
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(RoleModel::class, 'user_roles', 'user_id', 'role_id');
+    }
+
+    // Kiểm tra nếu user có vai trò cụ thể
+    public function hasRole(string $roleName): bool
+    {
+        return $this->roles->contains('name', $roleName);
     }
 }
