9db1c7c5dab714e9dfbb37c056c944adc9fa2fb9
1ien4nh <anhnt@goads.vn>
2025-02-20 10:44:34 +0700

    add edit role

--- resources/views/domains/role/index.blade.php
+++ resources/views/domains/role/index.blade.php
@@ -1,28 +1,72 @@
-@extends ('layouts.in')
+@extends('layouts.app')
 
-@section ('body')
-
-<form method="get">
-    <div class="sm:flex sm:space-x-4">
-        <div class="flex-grow mt-2 sm:mt-0">
-            <input type="search" class="form-control form-control-lg" placeholder="{{ __('role-index.filter') }}" data-table-search="#role-list-table" />
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ __('Quản lý vai trò') }}</h3>
         </div>
 
-        @if ($users_multiple)
+        <div class="card-body">
+            <!-- Form tìm kiếm -->
+            <form method="GET" action="{{ route('role.index') }}" class="mb-4">
+                <div class="row">
+                    <div class="col-md-4">
+                        <input type="text"
+                            name="search"
+                            class="form-control"
+                            placeholder="Tìm kiếm..."
+                            value="{{ $search }}">
+                    </div>
 
-        <div class="flex-grow mt-2 lg:mt-0">
-            <x-select name="user_id" :options="$users" value="id" text="name" placeholder="{{ __('device-index.user') }}" data-change-submit></x-select>
+                    <div class="col-md-2">
+                        <button class="btn btn-primary" type="submit">
+                            {{ __('Tìm kiếm') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <!-- Bảng dữ liệu -->
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>{{ __('Tên') }}</th>
+                            <th>{{ __('Mô tả') }}</th>
+                            <th>{{ __('Ngày tạo') }}</th>
+                            <th>{{ __('Thao tác') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($roles as $role)
+                        <tr>
+                            <td>{{ $role->id }}</td>
+                            <td>{{ $role->name }}</td>
+                            <td>{{ $role->description }}</td>
+                            <td>{{ \Carbon\Carbon::parse($role->created_at)->format('d/m/Y H:i') }}</td>
+                            <!-- <td>
+                                <!-- <a href="{{ route('role.edit', $role->id) }}"
+                                    class="btn btn-sm btn-primary">
+                                    {{ __('Sửa') }}
+                                </a> -->
+                            </td> -->
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="5" class="text-center">
+                                {{ __('Không có dữ liệu') }}
+                            </td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Phân trang -->
+            {{ $roles->links() }}
         </div>
-
-        @endif
-
-        <button type="button" class="sm:ml-4 mt-2 sm:mt-0 bg-white btn form-control-lg" data-notification-request data-notification-request-granted="{{ __('role-index.notifications-granted') }}" data-notification-request-denied="{{ __('role-index.notifications-denied') }}">{{ __('role-index.notifications-enable') }}</button>
     </div>
-</form>
-
-<div class="overflow-auto scroll-visible header-sticky">
-    <table id="role-list-table" class="table table-report sm:mt-2 font-medium font-semibold text-center whitespace-nowrap" data-table-sort data-table-pagination data-table-pagination-limit="10">
-    </table>
 </div>
-
-@stop
+@endsection
