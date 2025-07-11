132586666ddef53f7e1778e021ddff5f5fea0de8
Lito <lito@eordes.com>
2025-07-11 10:03:18 +0200

    Updated Translations

---
+++ resources/lang/pt_BR/alarm-type-fence.php
@@ -0,0 +1,7 @@
+<?php return [
+    'error' => [
+        'latitude' => 'A Latitude indicada não possui um valor válido',
+        'longitude' => 'A Longitude indicada não possui um valor válido',
+        'radius' => 'O Raio indicado não possui um valor válido',
+    ],
+];
